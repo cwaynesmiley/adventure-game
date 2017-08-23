41113df36169ac6654386eecb046b5a9242500fe
Wayne Smiley <cwaynesmiley@gmail.com>
2017-08-22 21:07:57 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-
+function playGame() {
+  var text;
+  var answer = document.getElementById("startQuestion").value;
+  
+  switch(answer){
+  case "Yes":
+    text="Awesome, let's play!";
+    break;
+  case "No":
+    text="Fine, be a bum!";
+    break;
+  default:
+    text="What you talking about Willis? Answer Yes or No!";
+    break;
+               }
+    document.getElementById("adventure-answer").innerHTML = text;
+};
